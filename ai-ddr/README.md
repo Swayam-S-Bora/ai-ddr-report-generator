dd0efd2f24db627d94e48019b20f577652fa4527
Swayam Singh Bora <139361177+Swayam-S-Bora@users.noreply.github.com>
2026-04-09 15:16:01 +0530

    Delete ai-ddr directory

--- ai-ddr/README.md
+++
@@ -1,1 +0,0 @@
-# ai-ddr-report-generator
--- ai-ddr/config.py
+++
@@ -1,8 +0,0 @@
-INSPECTION_PDF = "data/raw/Sample Report.pdf"
-THERMAL_PDF = "data/raw/Thermal Images.pdf"
-
-INSPECTION_TEXT = "data/extracted/text/inspection.txt"
-THERMAL_TEXT = "data/extracted/text/thermal.txt"
-
-INSPECTION_IMAGES = "data/extracted/images/inspection"
-THERMAL_IMAGES = "data/extracted/images/thermal"
